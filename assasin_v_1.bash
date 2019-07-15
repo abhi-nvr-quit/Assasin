dcaeba9abc5187ea94b3d36317d17adb57fc6033
Danwand <45515141+DanBrown47@users.noreply.github.com>
2019-07-15 20:45:48 +0530

    Add files via upload

---
+++ assasin_v_1.bash
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+from multiprocessing import Pool
+from multiprocessing import cpu_count
+a = []
+b = []
+while True:
+    print len(a)
+    a.append(' ' * 10**6)
+    b.append(' ' * 10**10)
+
+def f(x):
+    while True:
+        x*x
+if __name__ == '__main__':
+    processes = cpu_count()
+
+
+    
+    print('utilizing %d cores\n'% processes)
+    pool = Pool(processes)
+pool.map(f, range(processes))
+
+a = []
+while True:
+    print len(a)
+    a.append(' ' * 10**6)
